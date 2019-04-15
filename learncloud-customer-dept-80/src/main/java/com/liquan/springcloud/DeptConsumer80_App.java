937fab62118943eb102cb09a9667a2133b3806dc
liquanjava <178891876@qq.com>
2019-04-16 07:28:05 +0800

    ribbon

--- learncloud-customer-dept-80/src/main/java/com/liquan/springcloud/DeptConsumer80_App.java
+++ learncloud-customer-dept-80/src/main/java/com/liquan/springcloud/DeptConsumer80_App.java
@@ -1,11 +1,14 @@
 package com.liquan.springcloud;
 
+import com.liquan.springcloud.com.myrule.MyRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "LEARNCLOUD-DEPT",configuration = MyRule.class)
 public class DeptConsumer80_App {
 
 
--- learncloud-customer-dept-80/src/main/java/com/liquan/springcloud/cfgbeans/ConfigBean.java
+++ learncloud-customer-dept-80/src/main/java/com/liquan/springcloud/cfgbeans/ConfigBean.java
@@ -1,5 +1,8 @@
 package com.liquan.springcloud.cfgbeans;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RetryRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +15,11 @@ public class ConfigBean {
     @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public IRule getrule(){
+
+        return new RetryRule();
     }
 }
